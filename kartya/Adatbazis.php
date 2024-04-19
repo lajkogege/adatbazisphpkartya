dca8877813bc104a4cd570ca0e0f4d8b737b548e
lajkogege <lajkogege@gmail.com>
2024-04-19 10:19:50 +0200

    first commit

---
+++ kartya/Adatbazis.php
@@ -0,0 +1,83 @@
+<?php
+class Adatbazis {
+    //adattagok
+    private $host = "localhost";
+    private $felhasznaloNev = "root";
+    private $jelszo = "";
+    private $adatbazis = "magyarkartya";
+    private $kapcsolat;
+    //konstruktor
+    public function __construct()
+    {
+        $this->kapcsolat = new mysqli(
+            $this->host,
+            $this->felhasznaloNev,
+            $this->jelszo,
+            $this->adatbazis
+        );
+        $siker = 0;
+        if ($this->kapcsolat->connect_errno) {
+            $siker = "Nem sikerült a kapcsolat";
+        }
+        else
+            $siker = "Sikerült a kapcsolat";
+        //echo $siker;
+        $this->kapcsolat ->query("SET NAMES UTF8");
+    }
+    //metódusok
+    /*public function adatLeker($oszlop, $tabla){
+        $sql = "SELECT $oszlop FROM $tabla";
+        return $this->kapcsolat->query($sql);
+    }
+    public function megjelenit($matrix){
+        while ($sor = $matrix->fetch_row()) {
+            echo "<img src=\"forras/$sor[0]\" alt=\"forras/$sor[0]\">";
+        }
+    }*/
+
+    public function adatLeker2($oszlop1, $oszlop2, $tabla){
+        $sql = "SELECT $oszlop1 , $oszlop2 FROM $tabla";
+        return $this->kapcsolat->query($sql);
+    }
+    public function megjelenit2($matrix){
+        echo"<table> ";
+            echo "<tr>
+                    <th></th>
+                    <th></th>
+                </tr>";
+        while ($sor = $matrix->fetch_row()) {  
+            
+                echo"<tr>
+                        <td>$sor[0]</td>
+                        <td><img src=\"forras/$sor[1]\" alt=\"forras/$sor[0]\"></td>
+                        </tr>";
+        }
+        echo"</table>";
+            echo "<img src=\"forras/$sor[0]\" alt=\"forras/$sor[0]\">";
+    }
+
+    public function modosit($tabla, $oszlop, $regiErtek, $ujErtek){
+        $sql = "UPDATE $tabla SET $oszlop='$ujErtek' WHERE '$oszlop' = '$regiErtek'";
+        return $this->kapcsolat->query($sql);
+    }
+
+
+    public function rekordokSzama($tabla){
+        $sql = "SELECT * FROM $tabla";
+        return $this->kapcsolat->query($sql)->num_rows;
+    }
+    public function kartyaFeltolt(){
+        $szinOsszeg = $this->rekordokSzama("szin") + 1;
+        $formaOsszeg = $this->rekordokSzama("forma") + 1;
+        for ($szinIndex = 1; $szinIndex < $szinOsszeg; $szinIndex++) { 
+            for ($formaIndex = 1; $formaIndex < $formaOsszeg; $formaIndex++){
+                $sql = "INSERT INTO kartya(szinAzon, formaAzon) VALUES ('$szinIndex','$formaIndex')";
+                $this->kapcsolat->query($sql);
+            }
+        }
+    }
+    public function kapcsoltBezar(){
+        $this->kapcsolat->close();
+    }
+}
+?>
---
+++ kartya/index.php
@@ -0,0 +1,25 @@
+<?php
+    //import
+    include_once "Adatbazis.php";
+?>
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Magyar kártya</title>
+</head>
+<body>
+    <?php
+        $adatbazis = new Adatbazis();
+        //lekérdezések:
+        $adatbazis->modosit("szín", "nev", "zöld", "green");
+        $matrix = $adatbazis->adatLeker("kep", "szin");
+        $adatbazis->megjelenit($matrix);
+        if ($adatbazis->rekordokSzama("kartya") == 0) {
+            $adatbazis->kartyaFeltolt();
+        }
+        $adatbazis->kapcsoltBezar();
+    ?>
+</body>
+</html>
